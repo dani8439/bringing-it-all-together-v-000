a03ef3ed7f5ec970b22aa45be489b5e15a2611d4
Dani Schuhman <dani8439@aol.com>
2018-08-03 17:06:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -30,20 +30,20 @@ class Dog
     DB[:conn].execute(sql)
   end
 
-  # def self.new_from_db(row)
-  #   new_dog = self.new
-  #   new_dog.id = row[0]
-  #   new_dog.name = row[1]
-  #   new_dog.breed = row[2]
-  #   new_dog
-  # end
-
   def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    breed = row[2]
-    self.new(id: id, name: name, breed: breed)
+    new_dog = self.new
+    new_dog.id = row[0]
+    new_dog.name = row[1]
+    new_dog.breed = row[2]
+    new_dog
   end
+
+  # def self.new_from_db(row)
+  #   id = row[0]
+  #   name = row[1]
+  #   breed = row[2]
+  #   self.new(id: id, name: name, breed: breed)
+  # end
 
   def self.find_by_name(name)
     sql = <<-SQL
