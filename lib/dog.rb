1161a8b553e0e2790aebf53ec3cea37d0e1a55df
Dani Schuhman <dani8439@aol.com>
2018-08-03 17:06:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -38,6 +38,13 @@ class Dog
   #   new_dog
   # end
 
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
+  end
+
   def find_by_name(name)
     sql = <<-SQL
       SELECT * FROM dogs WHERE name = ? LIMIT 1
