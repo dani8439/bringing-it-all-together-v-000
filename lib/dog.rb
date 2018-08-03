66e03e5ae4a6a65c7df8bad31134250cb03336c3
Dani Schuhman <dani8439@aol.com>
2018-08-03 16:52:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,8 +40,7 @@ class Dog
 
   def find_by_name(name)
     sql = <<-SQL
-      SELECT * FROM dogs
-      WHERE ? = name 
+      SELECT * FROM dogs WHERE name = ? LIMIT 1 
     SQL
     DB[:conn].execute(sql)
   end
